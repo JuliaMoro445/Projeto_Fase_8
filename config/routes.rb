cc0ef17a8941a10b8c128983eb5fb101093db35b
andre-tlima <andre-tlima@hotmail.com>
2022-05-28 13:31:47 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
+    namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1) do
+
+      resources :teste
+
+    end
 
   end
 end
