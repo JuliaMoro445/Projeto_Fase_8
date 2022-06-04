2c6102b1d0c9db7fe54797d273fd2360bf8005b4
andre-tlima <andre-tlima@hotmail.com>
2022-06-04 13:08:50 +0000

    adicionar restrição de inversão para rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
+require "api_version_constraint"
+
 Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
-    namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1) do
-
+    namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
       resources :teste
 
     end
-
   end
 end
